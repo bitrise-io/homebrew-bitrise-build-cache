a3110ea80d1cba17bec7a18c2f07f48ba3b674ac
bitbot <letsbuild@bitrise.io>
2026-05-15 08:38:22 +0000

    brew formula update for bitrise-build-cache v2.6.7

--- Formula/bitrise-build-cache.rb
+++ Formula/bitrise-build-cache.rb
@@ -1,28 +1,28 @@
 class BitriseBuildCache < Formula
   desc "Bitrise Build Cache CLI — configure remote build cache for Gradle, Bazel, Xcode, and React Native"
   homepage "https://bitrise.io"
-  version "2.6.6"
+  version "2.6.7"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v2.6.6/bitrise-build-cache_2.6.6_darwin_arm64.tar.gz"
-      sha256 "eadfe510a249038e9fe0c52199ed0fbeef031514fd31bf354818fdffbab686a1"
+      url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v2.6.7/bitrise-build-cache_2.6.7_darwin_arm64.tar.gz"
+      sha256 "99be0b555f0e46864ee70cbd77311e177de72a2bb9ef45ae7102b4da74d073e1"
     end
     on_intel do
-      url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v2.6.6/bitrise-build-cache_2.6.6_darwin_amd64.tar.gz"
-      sha256 "1cf6fdac7abf89a1eeb5e1b743a97566f21a7f4cc95726b08c1d404a7c0745d7"
+      url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v2.6.7/bitrise-build-cache_2.6.7_darwin_amd64.tar.gz"
+      sha256 "29d05188007ae6f8e6dcf20bc1a2e88b6203833e7b55693b594f54bf146d2a79"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v2.6.6/bitrise-build-cache_2.6.6_linux_arm64.tar.gz"
-      sha256 "a2647cff8975ccd1b1a2fe01ab9ac4719cd47f0c76635cf7a2c2a6d0ced80d3c"
+      url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v2.6.7/bitrise-build-cache_2.6.7_linux_arm64.tar.gz"
+      sha256 "d3839615b6db8bbcd0902cc4b0d629b7cc97d653d87d9add693a4e3ed3be8b2f"
     end
     on_intel do
-      url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v2.6.6/bitrise-build-cache_2.6.6_linux_amd64.tar.gz"
-      sha256 "bc2d728cb04a04111b83bdbb6b7273c4aa632d41d17107776445b110ab09ed36"
+      url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v2.6.7/bitrise-build-cache_2.6.7_linux_amd64.tar.gz"
+      sha256 "89a88aa3f000ef4ca5da04577eb5afe94f5d3605db7c5d0dc295524d911bdeab"
     end
   end
 
