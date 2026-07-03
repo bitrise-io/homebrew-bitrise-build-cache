cc7dfe126100a6285e7d442ab756361f14dad076
bitbot <letsbuild@bitrise.io>
2026-07-03 15:51:53 +0000

    brew formula update for bitrise-build-cache v3.0.0

--- Formula/bitrise-build-cache.rb
+++ Formula/bitrise-build-cache.rb
@@ -7,22 +7,22 @@ class BitriseBuildCache < Formula
   on_macos do
     on_arm do
       url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v3.0.0/bitrise-build-cache_3.0.0_darwin_arm64.tar.gz"
-      sha256 "36de2f0f83b9ef97c8c76b56977da325f2cd94bc9e08fe48df89d809db150cb4"
+      sha256 "20584f7dd72197a7ee2e2cfe8e590aa98c1d8f8f8d29eeff31da1c1b7527c565"
     end
     on_intel do
       url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v3.0.0/bitrise-build-cache_3.0.0_darwin_amd64.tar.gz"
-      sha256 "65a8d5f374b511944efea6c216809d3d2a772652ddeac5374c0e9ae3a6686775"
+      sha256 "a6908a8c512b0e1fed026213ee7433d98ef491c83728db76dd1dc61a60dcbd09"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v3.0.0/bitrise-build-cache_3.0.0_linux_arm64.tar.gz"
-      sha256 "83f470876f8008d2f310b1c48d32b938cc1817735bc8ac1a8efdda3e95d91e88"
+      sha256 "130a7917a5b61f3edd85ebda8865b8ec3903b937e748f68fb9b428977aa2f09a"
     end
     on_intel do
       url "https://github.com/bitrise-io/bitrise-build-cache-cli/releases/download/v3.0.0/bitrise-build-cache_3.0.0_linux_amd64.tar.gz"
-      sha256 "56781a978934881743681c4fb8f41475f27d36e42d328fff95097a554d7774b4"
+      sha256 "e013e8f987849e50f281bde3ce8a1c4706c6380804a49f1d9e881023612fd9b3"
     end
   end
 
